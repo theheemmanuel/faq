8d94eceb2516a08d3c19f4d8a132e027fc418669
Emmanuel Oladiran <oladirane@gmail.com>
2024-01-30 23:30:26 +0100

    added minus sign

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ const quest = document.querySelectorAll(".qqss");
 quest.forEach((e) => {
   const answer = e.querySelector(".answer");
   const icon = e.querySelector(".icon");
+  const icon2 = e.querySelector(".icon2");
 
   icon.addEventListener("click", () => {
     answer.classList.toggle("active");
+    icon.classList.toggle("active");
+    icon2.classList.toggle("active");
+  });
+
+  icon2.addEventListener("click", () => {
+    answer.classList.toggle("active");
+    icon.classList.toggle("active");
+    icon2.classList.toggle("active");
   });
 });
